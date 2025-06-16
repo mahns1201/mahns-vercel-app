74907a2c545b8787d227e4fc992de88b40d3e7d6
서민혁 <mahns1201@gmail.com>
2025-06-16 15:00:18 +0900

    Fix post-card link

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,15 +1,8 @@
-import Link from 'next/link';
-
 export default function PostCard({ post }) {
   return (
     <>
-      <h2 className="text-2xl font-bold">
-        <Link
-          href={`/blog/${post.slug}`}
-          className="text-gray-900 dark:text-gray-100"
-        >
-          {post.title}
-        </Link>
+      <h2 className="text-2xl font-bold text-gray-900 dark:text-gray-100">
+        {post.title}
       </h2>
       <p className="text-gray-500 dark:text-gray-400">{post.summary}</p>
     </>
--- components/PostList.tsx
+++ components/PostList.tsx
@@ -7,24 +7,14 @@ export default function PostList({ posts }) {
   return (
     <>
       {posts.map((post) => (
-        <li key={post.slug} className="py-12">
-          <article>
-            <div className="xl:grid xl:grid-cols-4 xl:items-baseline">
-              <div />
-              <div className="xl:col-span-3">
-                <PostCard post={post} />
-                <div className="text-base font-medium">
-                  <Link
-                    href={`/blog/${post.slug}`}
-                    className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    aria-label={`Read more: "${post.title}"`}
-                  >
-                    Read more &rarr;
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </article>
+        <li key={post.slug} className="group">
+          <Link
+            href={`/blog/${post.slug}`}
+            className="block py-12"
+            aria-label={`Read more: "${post.title}"`}
+          >
+            <PostCard post={post} />
+          </Link>
         </li>
       ))}
     </>
