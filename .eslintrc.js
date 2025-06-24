d8c385d0057259aba8ec655557593e5147a7e63c
서민혁 <mahns1201@gmail.com>
2025-06-24 20:11:40 +0900

    Fix eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['node_modules', 'dist'],
+};
