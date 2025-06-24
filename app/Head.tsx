a8d64fe712d80d516e48348fbc977252a266493f
서민혁 <mahns1201@gmail.com>
2025-06-24 18:24:04 +0900

    Add google search console

--- app/Head.tsx
+++ app/Head.tsx
@@ -1,8 +1,10 @@
 const GA_ID = process.env.GA_ID || '';
+const GOOGLE_SEARCH_CONSOLE = process.env.GOOGLE_SEARCH_CONSOLE || '';
 
 const Head = () => {
   return (
     <head>
+      {/* Google Analytics */}
       <script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
@@ -17,6 +19,11 @@ const Head = () => {
             `,
         }}
       />
+
+      {/* Google Search Console */}
+      <meta name="google-site-verification" content={GOOGLE_SEARCH_CONSOLE} />
+
+      {/* Favicon */}
       <link
         rel="icon"
         type="image/png"
