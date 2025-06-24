4b887153299ffb73b5beeae3c4dbc4d583999987
서민혁 <mahns1201@gmail.com>
2025-06-24 20:11:52 +0900

    Lint

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,9 +1,10 @@
 const PostCard = ({ post }) => {
   return (
     <article className="flex flex-col sm:flex-row items-center gap-4 sm:gap-10 px-2 sm:px-4 xl:px-6">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src={post.thumbnail ? post.thumbnail : '/logo.png'}
-        alt={post.title}
+        alt={post.title ? post.title : '무제'}
         className="w-28 h-21 sm:w-32 sm:h-24 xl:w-40 xl:h-30 object-cover rounded-md flex-shrink-0"
       />
       <div className="sm:flex-1">
