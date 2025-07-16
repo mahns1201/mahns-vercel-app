044275d9bd3913ea3b47b2751c8fbd9be42dcfc8
서민혁 <mahns1201@gmail.com>
2025-07-16 22:24:47 +0900

    Add slugs for sitemap

---
+++ data/slugs.js
@@ -0,0 +1,10 @@
+const slugs = [
+  'toeic-speaking-150-strategy',
+  'dopamine-nation',
+  'mac-setup-with-homebrew',
+  'how-to-explain-clearly',
+  'iot-server-with-rabbitmq',
+  'build-blog-with-nextjs',
+];
+
+export default slugs;
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,14 @@
+const slugs = require('./data/slugs.js');
+
 module.exports = {
   siteUrl: 'https://mahns.vercel.app/',
   generateRobotsTxt: true,
   sitemapSize: 10000,
+
+  // 동적 수동 경로 추가
+  additionalPaths: async () => {
+    return slugs.default.map((slug) => ({
+      loc: `/posts/${slug}`,
+    }));
+  },
 };
