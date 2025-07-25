c451ed89feb51e3f72e34a0a46cf4f816c8d9706
서민혁 <mahns1201@gmail.com>
2025-07-25 10:03:26 +0900

    Fix a tag overlapping problem on html5

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,21 +1,30 @@
+'use client';
+
 import Link from 'next/link';
 import PostCard from './PostCard';
 import { Post } from '../types/post';
+import { useRouter } from 'next/navigation';
 
 const PostList = ({ posts }: { posts: Post[] }) => {
   if (!posts.length) return <li>No posts found.</li>;
 
+  const router = useRouter();
+
+  const handlePostClick = (slug: string) => {
+    router.push(`/posts/${slug}`);
+  };
+
   return (
     <>
       {posts.map((post) => (
-        <li key={post.slug} className="group">
-          <Link
-            href={`/posts/${post.slug}`}
+        <li key={post.slug} className="group cursor-pointer">
+          <div
+            onClick={() => handlePostClick(post.slug)}
             className="block py-6 hover:bg-gray-100 dark:hover:bg-gray-700"
             aria-label={`Read more: "${post.title}"`}
           >
             <PostCard post={post} />
-          </Link>
+          </div>
         </li>
       ))}
     </>
--- components/TagList.tsx
+++ components/TagList.tsx
@@ -1,10 +1,17 @@
+'use client';
+
 import Link from 'next/link';
 
 const TagList = ({ tags }: { tags: string[] }) => {
   return (
     <div className="flex flex-wrap gap-2">
       {tags.map((tag) => (
-        <Link key={tag} href={`/tags/${tag}`} className="hover:underline">
+        <Link
+          key={tag}
+          href={`/tags/${tag}`}
+          className="hover:underline"
+          onClick={(e) => e.stopPropagation()}
+        >
           <p>#{tag}</p>
         </Link>
       ))}
