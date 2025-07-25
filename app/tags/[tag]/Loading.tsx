0892da9e7c5e84caa64fdcc2416351bbc76a1ba7
서민혁 <mahns1201@gmail.com>
2025-07-25 15:28:24 +0900

    Style list view skeleton

--- app/tags/[tag]/Loading.tsx
+++ app/tags/[tag]/Loading.tsx
@@ -1,45 +1,28 @@
 const TagLoading = () => (
   <>
-    <div className="flex space-x-8 pt-6">
-      <div className="w-3/4">
-        <div className="flex flex-col animate-pulse">
-          <div className="flex space-x-4 mb-20">
-            <div className="w-60 h-40 bg-gray-300 dark:bg-gray-700 rounded" />
-            <div className="flex flex-col w-full">
-              <div className="h-22 w-1/2 bg-gray-300 dark:bg-gray-700 rounded" />
-              <div className="h-4 w-3/4 mt-2 bg-gray-200 dark:bg-gray-600 rounded" />
-              <div className="h-4 w-1/3 mt-2 bg-gray-200 dark:bg-gray-600 rounded" />
-              <div className="ml-auto h-4 w-24 mt-2 bg-gray-200 dark:bg-gray-600 rounded" />
-            </div>
+    <div className="flex">
+      <div className="flex flex-col w-full sm:flex-row items-center gap-4 animate-pulse">
+        <div className="w-full h-60 sm:w-54 sm:h-42 bg-gray-300 rounded-md flex-shrink-0" />
+        <div className="sm:flex-1 space-y-2 w-full">
+          <div className="h-6 bg-gray-300 rounded w-3/4" />
+          <div className="h-4 bg-gray-300 rounded w-full" />
+          <div className="h-4 bg-gray-300 rounded w-5/6" />
+          <div className="flex flex-wrap gap-2 mt-2">
+            <div className="h-6 w-16 bg-gray-300 rounded-full" />
+            <div className="h-6 w-20 bg-gray-300 rounded-full" />
+            <div className="h-6 w-12 bg-gray-300 rounded-full" />
           </div>
-          <div className="flex space-x-4 mb-20">
-            <div className="w-60 h-40 bg-gray-300 dark:bg-gray-700 rounded" />
-            <div className="flex flex-col w-full">
-              <div className="h-22 w-1/2 bg-gray-300 dark:bg-gray-700 rounded" />
-              <div className="h-4 w-3/4 mt-2 bg-gray-200 dark:bg-gray-600 rounded" />
-              <div className="h-4 w-1/3 mt-2 bg-gray-200 dark:bg-gray-600 rounded" />
-              <div className="ml-auto h-4 w-24 mt-2 bg-gray-200 dark:bg-gray-600 rounded" />
-            </div>
-          </div>
-          <div className="flex space-x-4 mb-20">
-            <div className="w-60 h-40 bg-gray-300 dark:bg-gray-700 rounded" />
-            <div className="flex flex-col w-full">
-              <div className="h-22 w-1/2 bg-gray-300 dark:bg-gray-700 rounded" />
-              <div className="h-4 w-3/4 mt-2 bg-gray-200 dark:bg-gray-600 rounded" />
-              <div className="h-4 w-1/3 mt-2 bg-gray-200 dark:bg-gray-600 rounded" />
-              <div className="ml-auto h-4 w-24 mt-2 bg-gray-200 dark:bg-gray-600 rounded" />
-            </div>
-          </div>
+          <div className="h-4 bg-gray-300 rounded w-1/3 ml-auto" />
         </div>
       </div>
-      <div className="w-1/4 py-6 mt-6 space-y-4">
-        <div className="h-6 w-1/3 bg-gray-300 dark:bg-gray-700 rounded" />
-        <div className="h-4 w-4/6 bg-gray-300 dark:bg-gray-700 rounded" />
-        <div className="h-4 w-3/6 bg-gray-200 dark:bg-gray-600 rounded" />
-        <div className="h-4 w-3/6 bg-gray-300 dark:bg-gray-700 rounded" />
-        <div className="h-4 w-3/6 bg-gray-200 dark:bg-gray-600 rounded" />
-        <div className="h-4 w-2/6 bg-gray-300 dark:bg-gray-700 rounded" />
-        <div className="h-4 w-5/8 bg-gray-300 dark:bg-gray-700 rounded" />
+
+      <div className="hidden xl:block w-44 pt-6 ml-10 animate-pulse">
+        <div className="h-6 bg-gray-300 rounded w-20 mb-4" />
+        <div className="space-y-2">
+          {Array.from({ length: 6 }).map((_, i) => (
+            <div key={i} className="h-6 bg-gray-300 rounded" />
+          ))}
+        </div>
       </div>
     </div>
   </>
