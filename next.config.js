52c7e87c90ab9de4846868d452d6853304657ffc
mahns <mahns1201@gmail.com>
2026-04-20 02:32:53 +0900

    enhance: delete unused settings

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   reactStrictMode: true,
   turbopack: {},
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
 };
